3180b9ab8292f3b64a6c89a411997a607c653783
SinnottTM <tim.sinnott86@gmail.com>
2021-05-25 13:52:44 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [currentAnswer, setCurrentAnswer] = useState("");
+  const [unrelatedState, setUnrelatedState] = useState(1);
+
+  const result = () => {
+    if (currentNumber === 0){
+      setCurrentAnswer("not yet...")
+    } else if (currentNumber % 3 === 0 && currentNumber % 5 === 0){
+      setCurrentAnswer("fizzbuzz")
+    } else if (currentNumber % 3 === 0) {
+      setCurrentAnswer("fizz")
+    } else if (currentNumber % 5 === 0) {
+      setCurrentAnswer("buzz")
+    } else if (currentNumber === 0){
+      setCurrentAnswer("not yet...")
+    } else {
+      setCurrentAnswer("not yet...");
+    }
+  }
+
+  useEffect(() => {
+    result()
+  }, [currentNumber])
+  
+  const handleInc = (evt) => {
+    evt.preventDefault();
+    setCurrentNumber(currentNumber + 1)
+  }
+
+  const handleDec = (evt) => {
+    evt.preventDefault();
+    setCurrentNumber(currentNumber - 1)
+  }
+
+  const unrelatedStateChange = () => {
+    let randomNum = Math.floor(Math.random() * 100)
+    setUnrelatedState(randomNum)
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fizz Buzz</h1>
+      <br></br>
+      <button value={currentNumber} onClick={handleDec}> - </button>
+      <button value={currentNumber} onClick={handleInc}> + </button>
+      <br></br>
+      <h2>Number is {currentNumber}</h2>
+      <br></br>
+      <h2>Answer is {currentAnswer}</h2>
+      <br></br>
+      <button value={unrelatedState} onClick={unrelatedStateChange}> Change </button>
+      <br></br>
+      <h2>Random number is {unrelatedState}</h2>
     </div>
   );
 }
---
+++ src/component/Fizzbuzz.js
@@ -0,0 +1,11 @@
+// if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+//       setAnswer("fizzbuz")
+//     } else if (inputNumber % 3 === 0) {
+//       setAnswer("fizz")
+//     } else if (inputNumber % 5 === 0) {
+//       setAnswer("buzz")
+//     } else {
+//       setAnswer(inputNumber);
+//     }
+
+// export default Fizzbuzz;
